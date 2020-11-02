84c6fd6da277282e57205edd97ec4990ae2c5172
Brunadisner <bruna.disner@gmail.com>
2020-11-01 21:56:20 -0300

    Arrumando quick

--- Trabalho2/quicksort.c
+++ Trabalho2/quicksort.c
@@ -3,17 +3,19 @@ int particiona (int *vet, int inicio, int fim) {
     int aux, esq, dir;
     esq = inicio + 1;
     dir = fim;
-    while(esq<dir){
-        while(vet[esq]<=pivo){
+    while(esq <= dir){
+        if(vet[esq] <= pivo){
             esq++;
         }
-        while (vet[dir] > pivo){
+        else if (vet[dir] > pivo){
             dir--;
         }
-        if(esq < dir){
+        else if(esq <= dir){
             aux = vet[esq];
             vet[esq] = vet[dir];
             vet[dir] = aux;
+            esq++;
+            dir--;
         }
         
         
@@ -25,10 +27,8 @@ int particiona (int *vet, int inicio, int fim) {
 
 void quicksort (int *vet, int inicio, int fim) {
     int pivo;
-    printf("Inicio %d \n fim %d \n",inicio,fim);
     if (inicio < fim) {
         pivo = particiona (vet, inicio, fim);
-        printf("pivo %d\n ",pivo);
         quicksort(vet, inicio, pivo-1);
         quicksort(vet, pivo+1, fim);
 
